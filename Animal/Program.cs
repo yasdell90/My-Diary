db7e6f61a4ceace01d385c87d5236b59eda975f7
Yasser <y-dl@outlook.com>
2019-02-18 16:54:31 +0800

    initial commit

---
+++ Animal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myPath = "c:\\users\\developer\\desktop\\myFile.txt";
+            if (!File.Exists(myPath))
+            {
+                File.Create(myPath);
+            }
+            var myReader = new StreamReader(myPath);
+            var content = string.Empty;
+            while (!myReader.EndOfStream)
+            {
+                content = myReader.ReadLine();
+                Console.WriteLine(content);
+            }
+            myReader.Close();
+            Console.WriteLine("enter text you want to save to your file");
+            var myInput = Console.ReadLine();
+            var myWriter = new StreamWriter(myPath);
+            myWriter.Write(myInput + "\n");
+            myWriter.Close();
+
+        }
+    }
+
+}
---
+++ MyDiary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyDiary.Startup))]
+namespace MyDiary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
